6f628d7b8a72f58ed837a4b682d5b246b260deac
Jonathan <jmtheron2@gmail.com>
2019-02-13 22:17:35 +0200

    Jonathan's commit

--- src/main/java/com/xyz/App.java
+++ src/main/java/com/xyz/App.java
@@ -9,10 +9,9 @@ public class App
     public static void main( String[] args ){
 
     System.out.println( "Hello Maven World!" );
-
     System.out.println( "Let the GIT Fun begin!!!" );
-
-        System.out.println( "Playground Commit" );
+    System.out.println( "Playground Commit" );
+    System.out.println( "Jonathan was here");
 
     }
 }
